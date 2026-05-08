7c692b12b35318d400123043ba82854050ee802e
Colton Allen <colton.allen@sentry.io>
2026-05-08 13:22:30 -0500

    Add type

--- src/scm/types.py
+++ src/scm/types.py
@@ -1183,7 +1183,7 @@ class MinimizeCommentProtocol(Protocol):
 @runtime_checkable
 class ResolveReviewThreadProtocol(Protocol):
     def get_thread_id_from_review_comment_unique_id(
-        self, pull_request_id: str, review_comment_unique_id
+        self, pull_request_id: str, review_comment_unique_id: str
     ) -> str | None: ...
     def resolve_review_thread(self, pull_request_id: str, thread_id: str) -> None: ...
 
