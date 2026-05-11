4e595f7d8ece5c93b412945101c40a729007a6a2
Vincent Jacques <vincent@vincent-jacques.net>
2026-05-11 17:26:12 +0200

    Support SCM listeners for Seer code review, for GitLab (#38)
    This is a companion to https://github.com/getsentry/sentry/pull/113032.

--- src/scm/types.py
+++ src/scm/types.py
@@ -528,6 +528,7 @@ type PullRequestAction = Literal[
 
 
 class PullRequestEventData(TypedDict):
+    repository_id: str
     id: str
     title: str
     description: str | None
@@ -535,6 +536,7 @@ class PullRequestEventData(TypedDict):
     base: PullRequestBranch
     is_private_repo: bool
     author: Author | None
+    draft: bool
 
 
 # Basic protocols
